c1c154a6d1022770a2b5bfc47a1dfbb478f6975e
LuisEdua <lefarrera@gmail.com>
2024-06-03 23:33:19 -0600

    [UPDATE] endpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/v1/practica', (req, res) => {
+app.get('/api/v1/welcome', (req, res) => {
   res.send('Hello, World! \n Practica 2024');
 });
 
