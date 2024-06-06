a20d3650f97718988724142f3edd4490c177a86f
LuisEdua <lefarrera@gmail.com>
2024-06-06 01:14:03 -0600

    [UPDATE] message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hello, World! \n Practica 2024');
+  res.send('Hello, World con un nuevo mensaje!');
 });
 
 app.listen(port, () => {
