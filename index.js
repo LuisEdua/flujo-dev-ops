10b5707a3c26854b7473904888c22258d922ff7d
LuisEdua <lefarrera@gmail.com>
2024-06-07 01:17:38 -0600

    [UPDATE] Message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hello, World con un nuevo mensaje!');
+  res.send('Hello, Docker!');
 });
 
 app.listen(port, () => {
